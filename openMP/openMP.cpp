0f4bcb2f9be3c386aa25b72f1ce3b805104b4b40
Dylan <dylan.suazo2101@alumnos.ubiobio.cl>
2023-12-02 19:01:22 -0300

    Inclusion de comentarios en openMP.cpp y correccion en readmes

--- openMP/openMP.cpp
+++ openMP/openMP.cpp
@@ -1,8 +1,3 @@
-/*
-g++ -o versionOpenMP versionOpenMP.cpp -std=c++11 `pkg-config --cflags --libs opencv4` -fopenmp
-/versionOpenMP imagen_rayo.jpg imagen_rayo_gris.jpg 4
-*/
-
 #include <omp.h>
 #include <iostream>
 #include <opencv2/opencv.hpp>
@@ -12,18 +7,21 @@ using namespace cv;
 using namespace std;
 using namespace std::chrono;
 
-//Codigo que ocupa la API OPenMP para hacer version1.cpp
 int main(int argc, char *argv[]) {
+    // Verificación de argumentos en la línea de comandos
     if(argc != 4){
         cout << "Uso: " << argv[0] << " <imagen_entrada> <imagen_salida> <numero_hilos>\n";
         return -1;
     }
 
+    // Almacenamiento de argumentos en variables
     string nombreImagenEntrada = argv[1];
     string nombreImagenSalida = argv[2];
     int numHilos = atoi(argv[3]);
 
     cout << "Imagen de entrada: " << nombreImagenEntrada << endl;
+
+    // Lectura de la imagen desde el archivo
     Mat image = imread(nombreImagenEntrada, IMREAD_COLOR);
 
     // Verificar si la imagen se cargó correctamente
@@ -32,23 +30,33 @@ int main(int argc, char *argv[]) {
         return -1;
     }
 
+    // Creación de una imagen en escala de grises
     Mat grayImage(image.rows, image.cols, CV_8UC1);
+
+    // Comienzo de la medición del tiempo de ejecución
     auto start_time = high_resolution_clock::now();
-    // Recorrer la imagen y mostrar los valores de los píxeles (en BGR)
+
+    // Bucle para convertir la imagen a escala de grises (paralelizado con OpenMP)
     #pragma omp parallel for num_threads(numHilos)
     for(int r=0; r<image.rows; r++) {
         for(int c=0; c<image.cols; c++) {
             Vec3b pixel = image.at<Vec3b>(r,c);
-            // x:B y:G z:R
+            // Cálculo de luminosidad usando los canales R, G y B
             uchar luminosity = 0.3*pixel[2] + 0.59*pixel[1] + 0.11*pixel[0];
+            // Asignación de la luminosidad a la imagen en escala de grises
             grayImage.at<uchar>(r,c) = luminosity;
         }
     }
 
+    // Escritura de la imagen en escala de grises en un archivo
     imwrite(nombreImagenSalida, grayImage);
     cout << "Imagen en gris creada con exito\n";
+
+    // Fin de la medición del tiempo de ejecución y cálculo de la duración
     auto end_time = high_resolution_clock::now();
     auto duration = duration_cast<seconds>(end_time - start_time);
+
+    // Mensaje con el tiempo de ejecución
     cout << "Tiempo de ejecucion: " << duration.count() << " segundos" << endl;
 
     return 0;
